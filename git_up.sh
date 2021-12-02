dec7021e26c75948ab24c37816b5ecf172cf60aa
tutu-kang <123kangpeng@gmail.com>
2021-12-01 20:07:49 -0600

    git

--- git_up.sh
+++ git_up.sh
@@ -1,3 +1,1 @@
-git remote remove origin
-git remote add origin https://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@github.com/tutu-kang/branchynet-gpu
-git push
+git remote set-url origin git@github.com:tutu-kang/branchynet-gpu.git
